a4843b77c5911c2db184239bf91ff8a3ef692897
Rachel Fingerhut <fingerhutrachel@gmail.com>
2023-08-23 11:31:12 -0700

    Set up server API

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,7 +1,7 @@
 const env = process.env;
 
 export const PORT = env.PORT ?? "8080";
-export const HOST = env.HOST ?? "0.0.0.0";
+export const HOST = env.HOST ?? "127.0.0.1";
 export const SERVER_URL = `http://${HOST}:${PORT}`;
 
 export const MONGODB_URI =
