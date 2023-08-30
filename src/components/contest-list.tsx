0ff509119d7b9dfae56832ba98af7ac819adda4e
Rachel Fingerhut <fingerhutrachel@gmail.com>
2023-08-30 09:11:11 -0700

    Add html for contest name proposals to page

--- src/components/contest-list.tsx
+++ src/components/contest-list.tsx
@@ -7,10 +7,12 @@ import Header from "./header";
 // displays ALL contests-previews in database.
 const ContestList = ({initialContests, onContestClick}) => {
   //once the data is rendered, update (use a state element).
+  // initial contests has a default value of an empty array if no initialContests exists.
   const [contests, setContests] = useState(initialContests ?? [],);
   
   // load data to be displayed (runs on render)
   useEffect(() => {
+    // only fetch if there are no initial contests
     if (!initialContests){
       // need an http client (use fetch or axios)
       fetchContestList().then((contests) => {
